737b8f9e1e5c232ac11f1a7c7ec6017b300cc98f
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-17 12:45:33 +0530

    feat(client): add Best Seller section to display top-selling products
    - Created `BestSeller` component to highlight top-selling products based on the `bestseller` property from `ShopContext`.
    - Used the `useContext` hook to access products and filtered them to display only those marked as bestsellers.
    - Displayed the first 5 best-selling products in a responsive grid, with column adjustments for different screen sizes (2 to 5 columns).
    - Integrated the `Title` and `ProductItem` components for consistent layout and styling with other product sections, enhancing visibility of popular items.

---
+++ client/src/components/BestSeller.jsx
@@ -0,0 +1,34 @@
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import ProductItem from "./ProductItem";
+import Title from "./Title";
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext);
+  const [bestSellers, setBestSellers] = useState([]);
+  useEffect(() => {
+    const bestProducts = products.filter((product) => product.bestseller);
+    setBestSellers(bestProducts.slice(0, 5));
+  }, []);
+  return (
+    <div className="my-10">
+      <div className="text-center py-8  text-xl sm:text-2xl lg:text-3xl">
+        <Title text1={"BEST "} text2={"SELLERS"} />
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4 gap-y-6">
+        {bestSellers.map((item) => (
+          <ProductItem
+            key={item._id}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
