f830195326e1062304012552c14c8bf2a987cc9b
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-17 12:28:35 +0530

    feat(client): add Latest Collection section to display featured products
    - Created `LatestCollection` component to showcase the 10 most recent products from the global `ShopContext`.
    - Used the `useContext` hook to access products and filtered the first 10 items for display.
    - Rendered the products in a responsive grid layout, adjusting the number of columns based on screen size (2 to 5 columns).
    - Integrated the `Title` and `ProductItem` components to structure and style the section, enhancing the presentation of featured products.

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { assets } from "../assets/assets";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col md:flex-row border border-gray-400">
+    <div className="flex flex-col md:flex-row border border-gray-400 mt-4">
       {/*Hero left side */}
       <div className="w-full md:w-1/2 flex items-center justify-center py-10 md:py-0">
         <div className="text-[#414141]">
---
+++ client/src/components/LatestCollection.jsx
@@ -0,0 +1,34 @@
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import ProductItem from "./ProductItem";
+import Title from "./Title";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+  return (
+    <div className="my-10">
+      <div className="text-center py-8  text-xl sm:text-2xl lg:text-3xl">
+        <Title text1={"LATEST "} text2={"COLLECTIONS"} />
+      </div>
+      {/* rendering products*/}
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item) => (
+          <ProductItem
+            key={item._id}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
