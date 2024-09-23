0bd5f510df7e8813a2c7bb32b7c66b9747bd86c4
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-23 11:00:32 +0530

    feat(client): add login and sign-up functionality on login page
    - Created `Login` component with toggle functionality for "Sign In" and "Sign Up" forms.
    - Added form fields for email and password, with an additional name field for sign-up.
    - Implemented dynamic switch between "Login" and "Sign Up" forms based on user interaction.
    - Included "Forgot your password?" and "Create account" links to enhance user experience.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -39,11 +39,15 @@ const Navbar = () => {
         />
 
         <div className="group relative">
-          <img
-            src={assets.profile_icon}
-            alt="profile"
-            className="w-5 cursor-pointer"
-          />
+          <Link to="/login">
+            {" "}
+            <img
+              src={assets.profile_icon}
+              alt="profile"
+              className="w-5 cursor-pointer"
+            />
+          </Link>
+
           <div className="hidden group-hover:block absolute right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500">
               <p className=" cursor-pointer hover:text-black">My Profile</p>
