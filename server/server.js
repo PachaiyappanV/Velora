193bc5c8f2e55947c8c6ece939782a83f9b44bfa
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-23 12:36:36 +0530

    chore(server): implement MongoDB connection setup
    - Added `mongoose` for MongoDB connection management in the server.
    - Created a `connectDB` function to establish a connection using the provided MongoDB URI.
    - Exported the `connectDB` function for use in the main server file.
    - Prepared the database setup for future use with MongoDB models and queries.

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//database
+const connectDB = require("./db/connect");
+
 //rest of the packages
 const cors = require("cors");
 const morgan = require("morgan");
+const { toDate } = require("validator");
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -16,4 +20,17 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("Database connected successfully");
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
