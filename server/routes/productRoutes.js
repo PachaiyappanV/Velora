0b597d5f6d6f9aaf0a83032ecb0a9bdb4ac1b8a6
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-28 21:41:43 +0530

    feat(server): implement file upload handling and update product routes
    - Integrated `multer` to handle multiple image uploads with disk storage configuration.
    - Set up middleware to handle image uploads in `/addProduct` route with support for 4 images.
    - Updated `/addProduct` route to accept `image1`, `image2`, `image3`, and `image4` as fields.
    - Ensured that uploaded files retain their original filenames during the upload process.

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../middleware/multer");
 const {
   addProduct,
   listProducts,
   removeProduct,
   singleProduct,
 } = require("../controllers/productController");
+const upload = require("../middleware/multer");
 
-router.post("/addProduct", addProduct);
+router.post(
+  "/addProduct",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
 router.get("/listProducts", listProducts);
 router.delete("/removeProduct", removeProduct);
 router.get("/singleProduct", singleProduct);
