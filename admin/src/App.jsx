ca4e2a033f637b8902d8bb1103d1f42df475b67a
PachaiyappanV <pachaiyappan394@gmail.com>
2024-10-02 12:42:12 +0530

    feat(admin): set up routing and layout for admin panel
    - Added `Navbar` and `Sidebar` components to the admin panel for better navigation and layout.
    - Configured routes using `react-router-dom` for pages: `Add`, `List`, and `Orders`.
    - Structured layout with responsive sidebar and main content area for a clean user experience.
    - Applied basic TailwindCSS styling for overall UI consistency.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,3 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Admin panel</h1>;
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <>
+        <Navbar />
+        <div className="flex w-full">
+          <Sidebar />
+          <div
+            className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 
+          text-base"
+          >
+            <Routes>
+              <Route path="/add" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
+      </>
+    </div>
+  );
 }
