f57c1166952d0ef85229d77ed4ccd2f2b148f472
PachaiyappanV <pachaiyappan394@gmail.com>
2024-10-10 04:51:16 +0530

    feat(server): add Order schema for managing user orders
    - Created `OrderSchema` to store user orders, including user ID, products, and total amount.
    - Added fields to capture the shipping address as an object and the payment method.
    - Implemented default values for order status (`"Order placed"`) and payment status (set to `false` initially).
    - Included a `date` field to store the order creation timestamp.

---
+++ server/models/Order.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const OrderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  products: {
+    type: Array,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: Object,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "Order placed",
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  payment: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  date: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
