9b700f9dfd644bb9b93ceb98038039a0510584d8
PachaiyappanV <pachaiyappan394@gmail.com>
2024-10-10 03:40:47 +0530

    feat(server): implement cart operations for adding, updating, and retrieving user carts
    - Added logic to `addToCart` controller to handle adding products with specific sizes to the user's cart.
    - Implemented `updateCart` controller to update the quantity of items in the cart based on product ID and size.
    - Developed `getUserCart` controller to retrieve the cart data for the authenticated user.
    - Ensured all operations interact with the database to store and manage cart data per user.

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,13 +1,67 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
 const addToCart = async (req, res) => {
-  res.send("add to cart");
+  const { userId } = req.user;
+  const { productId, size } = req.body;
+
+  if (!productId || !size) {
+    throw new BadRequestError("Please provide product id and size");
+  }
+
+  const userData = await User.findById(userId);
+  let cartData = await userData.cartData;
+
+  if (cartData[productId]) {
+    if (cartData[productId][size]) {
+      cartData[productId][size] += 1;
+    } else {
+      cartData[productId][size] = 1;
+    }
+  } else {
+    cartData[productId] = {};
+    cartData[productId][size] = 1;
+  }
+
+  await User.findByIdAndUpdate(userId, { cartData });
+
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    message: "product added to cart successfully",
+  });
 };
 
 const updateCart = async (req, res) => {
-  res.send("update cart");
+  const { userId } = req.user;
+  const { productId, size, quantity } = req.body;
+
+  if (!productId || !size || !quantity) {
+    throw new BadRequestError("Please provide product id, size and quantity");
+  }
+
+  const userData = await User.findById(userId);
+  let cartData = await userData.cartData;
+
+  cartData[productId][size] = quantity;
+
+  await User.findByIdAndUpdate(userId, { cartData });
+
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    message: "cart updated successfully",
+  });
 };
 
 const getUserCart = async (req, res) => {
-  res.send("get user cart");
+  const { userId } = req.user;
+  const userData = await User.findById(userId);
+  const cartData = await userData.cartData;
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    data: {
+      cartData,
+    },
+  });
 };
 
 module.exports = {
