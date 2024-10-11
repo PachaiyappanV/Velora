8a9e976c4c630c5f6be93666daa73307766b74d9
PachaiyappanV <pachaiyappan394@gmail.com>
2024-10-11 08:53:47 +0530

    feat(client): implement dynamic order data fetching and display on orders page
    - Added `getOrders` function to fetch user orders from the server using Axios, with authorization via JWT token.
    - Updated state to store order data, including product status, payment method, and order date.
    - Refactored the UI to dynamically display order details such as quantity, size, payment method, and status.
    - Incorporated error handling with toast notifications for failed requests or unexpected errors.

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,16 +1,53 @@
-import { useContext } from "react";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import Title from "../components/Title";
 import { ShopContext } from "../context/ShopContext";
 
 const Orders = () => {
-  const { products, currency } = useContext(ShopContext);
+  const { apiUrl, token, currency } = useContext(ShopContext);
+  const [orderData, setOrderData] = useState([]);
+
+  const getOrders = async () => {
+    try {
+      const { data } = await axios.get(`${apiUrl}/api/order/userOrders`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      let allOrders = [];
+      data.data.orders.map((order) => {
+        order.products.map((product) => {
+          product["status"] = order.status;
+          product["payment"] = order.payment;
+          product["paymentMethod"] = order.paymentMethod;
+          product["date"] = order.date;
+
+          allOrders.push(product);
+        });
+      });
+
+      setOrderData(allOrders.reverse());
+    } catch (error) {
+      if (error.response.data.status === "fail") {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("Something went wrong please try again later");
+      }
+    }
+  };
+  useEffect(() => {
+    if (token) {
+      getOrders();
+    }
+  }, [token]);
   return (
     <div className="border-t pt-16">
       <div className="flex text-2xl">
         <Title text1={"MY "} text2={"ORDERS"} />
       </div>
       <div className="">
-        {products.slice(1, 4).map((item, index) => (
+        {orderData.map((item, index) => (
           <div
             key={index}
             className="py-4 border-t border-b text-gray-700 flex flex-col 
@@ -20,25 +57,37 @@ const Orders = () => {
               <img className="w-16 sm:w-20" src={item.image[0]} alt="" />
               <div>
                 <p className="sm:text-base font-medium">{item.name}</p>
-                <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
+                <div className="flex items-center gap-3 mt-1 mb-1 text-base text-gray-700">
                   <p>
                     {currency}
                     {item.price}
                   </p>
-                  <p>Quantity:1</p>
-                  <p>Size: M</p>
+                  <p>Quantity: {item.quantity}</p>
+                  <p>Size: {item.size}</p>
                 </div>
                 <p>
-                  Date<span className=" text-gray-400">25,jul,2024</span>
+                  Date:
+                  <span className=" text-gray-400">
+                    {" " + new Date(item.date).toDateString()}
+                  </span>
+                </p>
+                <p>
+                  Payment:
+                  <span className=" text-gray-400">
+                    {" " + item.paymentMethod.toUpperCase()}
+                  </span>
                 </p>
               </div>
             </div>
             <div className="md:w-1/2 flex justify-between">
               <div className="flex items-center gap-2">
                 <p className="min-w-2 h-2 rounded-full bg-green-400"></p>
-                <p className="text-sm md:text-base">Ready to ship</p>
+                <p className="text-sm md:text-base">{item.status}</p>
               </div>
-              <button className="border px-4 py-2 text-sm font-medium rounded-sm ">
+              <button
+                onClick={getOrders}
+                className="border px-4 py-2 text-sm font-medium rounded-sm "
+              >
                 Track order
               </button>
             </div>
