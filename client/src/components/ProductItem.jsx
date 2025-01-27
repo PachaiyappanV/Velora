e45cb23411d616533fb8719b1a18a73789c07d6a
PachaiyappanV <pachaiyappan394@gmail.com>
2025-01-27 22:58:17 +0530

    feat(client): add smooth scrolling for product navigations

--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -5,7 +5,11 @@ import { ShopContext } from "../context/ShopContext";
 const ProductItem = ({ id, image, name, price }) => {
   const { currency } = useContext(ShopContext);
   return (
-    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+    <Link
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      className="text-gray-700 cursor-pointer"
+      to={`/product/${id}`}
+    >
       <div className=" overflow-hidden">
         <img
           src={image[0]}
